5486050a1b73cb66aba28f4913dd79da5ad7a732
문정현 <58593218+pure-night@users.noreply.github.com>
2024-01-11 16:53:16 +0900

    Merge pull request #5 from rtanunity3/feat-PHT
    Feat pht

--- Play/Monster.cs
+++ Play/Monster.cs
@@ -3,33 +3,33 @@ namespace textdungeon.Play
     public class Monster : ICharacter
     {
         public string Name { get; }
-        public int AttPow { get; }
+        public int MonsterID {  get; }
+        public int AttPow { get; set; }
         public int Health { get; set; }
-        public int Gold { get; }
-        public int Level { get; }
+        public int Gold { get; set; }
+        public int Level { get; set; }
         public bool IsDead => Health <= 0;
+        public int PlusAttPow { get; set; }
+        public int PlusHealth { get; set; }
+        public int PlusGold { get; set; } // TODO 보상골드를 랜덤값으로 설정할수있도록 수정
 
-        public Monster(string name, int health, int attPow, int gold, int level)
+        public Monster(string name, int monsterid, int health, int attPow, int gold, int level)
         {
             Name = name;
+            MonsterID = monsterid;
             Health = health;
             AttPow = attPow;
             Gold = gold;
             Level = level;
+            PlusAttPow = 0;
+            PlusHealth = 0;
+            PlusGold = 0;
         }
 
         public void TakeDamage(int damage)
         {
             Health = Math.Max(Math.Min((Health - damage), 100), 0);
         }
-    }
-
-    public class Goblin : Monster
-    {
-        public Goblin(string name, int health, int attack, int gold, int level) : base(name, health, attack, gold, level) { }
-    }
-    public class Dragon : Monster
-    {
-        public Dragon(string name, int health, int attack, int gold, int level) : base(name, health, attack, gold, level) { }
+        public virtual void LevelScailing(int level) { }
     }
 }
